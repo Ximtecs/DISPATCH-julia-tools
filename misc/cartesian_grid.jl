6459f3e92b4879e3b383043615d21fa719d0a939
ximtecs <michael.haahr101293@gmail.com>
2024-09-26 10:34:38 +0200

    [FIX] cartesian grid now properly sets x, y, and z

--- misc/cartesian_grid.jl
+++ misc/cartesian_grid.jl
@@ -13,9 +13,20 @@ function get_xyz(Snapshot_meta :: Snapshot_metadata, drop_dims :: Bool)
     y = zeros(mem_size...)
     z = zeros(mem_size...)
 
-    x[:,:,:] .= collect(range(ORIGIN[1] + 0.5 * ds[1], stop=ORIGIN[1] + BOX[1] - 0.5*ds[1], length=mem_size[1]))
-    y[:,:,:] .= collect(range(ORIGIN[2] + 0.5 * ds[2], stop=ORIGIN[2] + BOX[2] - 0.5*ds[2], length=mem_size[2]))
-    z[:,:,:] .= collect(range(ORIGIN[3] + 0.5 * ds[3], stop=ORIGIN[3] + BOX[3] - 0.5*ds[3], length=mem_size[3]))
+
+    x_range = collect(range(ORIGIN[1] + 0.5 * ds[1], stop=ORIGIN[1] + BOX[1] - 0.5*ds[1], length=mem_size[1]))
+    y_range = collect(range(ORIGIN[2] + 0.5 * ds[2], stop=ORIGIN[2] + BOX[2] - 0.5*ds[2], length=mem_size[2]))
+    z_range = collect(range(ORIGIN[3] + 0.5 * ds[3], stop=ORIGIN[3] + BOX[3] - 0.5*ds[3], length=mem_size[3]))
+
+    for i in 1:mem_size[1]
+        for j in 1:mem_size[2]
+            for k in 1:mem_size[3]
+                x[i,j,k] = x_range[i]
+                y[i,j,k] = y_range[j]
+                z[i,j,k] = z_range[k]
+            end
+        end
+    end
             
     if drop_dims
         x = drop_unit_dims(x)
