7f3b4fe26a16c0a0bf7bea27fe68d280bb67b7b0
ximtecs <michael.haahr101293@gmail.com>
2025-03-12 08:31:38 +0100

    [FIX] value parsing now properly works

--- IO/Value_parsing.jl
+++ IO/Value_parsing.jl
@@ -12,29 +12,44 @@ function parse_array(value::String)
             push!(parsed_elements, element)
         end
     end
-    return join(parsed_elements, ",")[1:end-1]
+    res = join(parsed_elements, ",")
+    if (res[end] == ',') 
+        return res[1:end-1]
+    else
+        return res
+    end
 end
 
 
 function parse_value(value::String, type::DataType)
-    if type == Int
-        return parse(Int, strip(value, ','))
-    elseif type == Float64
-        return parse(Float64, strip(value, ','))
-    elseif type == Bool
-        return strip(value, ',') == "T"
-    elseif type == String
-        return strip(strip(value, ','), ''')
-    elseif type == Vector{Int}
-        array_str = parse_array(value)
-        return [parse(Int, val) for val in split(array_str, ',')]
-    elseif type == Vector{Float64}
-        array_str = parse_array(value)
-        return [parse(Float64, val) for val in split(array_str, ',')]
-    elseif type == Vector{Bool}
-        array_str = parse_array(value)
-        return [val == "T" for val in split(array_str, ',')]
-    else
-        error("Unsupported data type: $type")
+    try
+        if type == Int
+            return parse(Int, strip(value, ','))
+        elseif type == Float64
+            return parse(Float64, strip(value, ','))
+        elseif type == Bool
+            return strip(value, ',') == "T"
+        elseif type == String
+            return strip(strip(value, ','), ''')
+        elseif type == Vector{Int}
+            if value == ""
+                return Int[]
+            else
+                array_str = parse_array(value)
+                return [parse(Int, val) for val in split(array_str, ',')]
+            end
+        elseif type == Vector{Float64}
+            array_str = parse_array(value)
+            return [parse(Float64, val) for val in split(array_str, ',')]
+        elseif type == Vector{Bool}
+            array_str = parse_array(value)
+            return [val == "T" for val in split(array_str, ',')]
+        else
+            error("Unsupported data type: $type")
+        end
+    catch e
+        println("Error parsing value: '$value'")
+        println("with type: '$type'")
+        rethrow(e)
     end
 end
